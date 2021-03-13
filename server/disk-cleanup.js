6dd62b33714ffe74b61736a010d17b76f2a96211
Anubhav Gupta <anubhav200@gmail.com>
2021-03-13 23:11:12 +0530

    fixed disk space and added new library

--- server/disk-cleanup.js
+++ server/disk-cleanup.js
@@ -1,17 +1,24 @@
 const util = require('util');
-var ds = require('fd-diskspace');
 const path  = require('path');
 const fs = require('fs');
-
-const diskspace = util.promisify(ds.diskSpace);
+const checkDiskSpace = require('check-disk-space');
+const os = require('os');
 const recordingDirectory = path.join(__dirname, 'public', 'recordings');
 
 // Async
 async function getFreeDiskSpace() {
-    const { total:{ free }}  = await diskspace();
-    let remainingSpaceInGB = free/Math.pow(1024, 2);
+    let free;
+    if(os.platform() === 'linux') {
+        const data =  await  checkDiskSpace("/home/pi/cam/server");
+       free =  data.free;
+    } else {
+       const data =  await  checkDiskSpace("C:/");
+       free =  data.free;
+    }
+    let remainingSpaceInGB = free/Math.pow(1024, 3);
     return remainingSpaceInGB;
 }
+
 
 async function performCleanup(interval, MIN_BUFFER_DISK_SPACE_IN_GB) {
     console.log("CleanUp started:");
@@ -19,14 +26,15 @@ async function performCleanup(interval, MIN_BUFFER_DISK_SPACE_IN_GB) {
     const files = await fs.promises.readdir(recordingDirectory);
     const filesStatsPromises = files
                             .map(file => fs.promises.stat(path.join(recordingDirectory, file)));
-        
+
     let filesStatsResults = await Promise.all(filesStatsPromises);
-    
+
     filesStatsResults = Array.from(filesStatsResults
         .map((stats, index) => [stats.ctimeMs, files[index]])
         .sort((a,b) => b[0] - a[0]));
         console.log("Remaining Disk space:", remainingSpaceInGB);
     while(remainingSpaceInGB <= MIN_BUFFER_DISK_SPACE_IN_GB && (filesStatsResults.length >= 2)) {
+        console.log('in');
         const [_, fileName] = filesStatsResults.pop();
         fs.unlinkSync(path.join(recordingDirectory, fileName));
         remainingSpaceInGB = await getFreeDiskSpace();
@@ -35,6 +43,7 @@ async function performCleanup(interval, MIN_BUFFER_DISK_SPACE_IN_GB) {
     console.log("CleanUp completed:");
     setTimeout(()=>performCleanup(interval, MIN_BUFFER_DISK_SPACE_IN_GB), interval);
 }
+
 
 module.exports = {
     performCleanup
